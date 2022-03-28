891c504955b72114c450a5fa536769106939ac59
root <cruau.david@gmail.com>
2022-03-28 15:41:59 +0200

    Few minor changes (icon, .desktop file...)

--- polymer/eduke32/opk/eduke32.sh
+++ polymer/eduke32/opk/eduke32.sh
@@ -4,13 +4,13 @@ if [ ! -e /usr/lib/libvorbis.so.0 ] || [ ! -e /usr/lib/libvorbisfile.so.3 ]; the
 	rw
 	cp -f libvorbis.so.0 /usr/lib
 	cp -f libvorbisfile.so.3 /usr/lib
-	./eduke32 "$1" &
+	ro
+	./eduke32 &
 	pid record $!
 	wait $!
 	pid erase
-	ro
 else
-	./eduke32 "$1" &
+	./eduke32 &
 	pid record $!
 	wait $!
 	pid erase
