655685a1c5b030c7f94276edab2ccc8aa131efc8
root <cruau.david@gmail.com>
2022-06-18 11:59:25 +0200

    Fixed Instant Play with the shareware version by copying the file in /mnt/FunKey/.eduke32

--- polymer/eduke32/opk/eduke32.sh
+++ polymer/eduke32/opk/eduke32.sh
@@ -2,9 +2,16 @@
 # Detect whether libvorbis libs are already there then copy them if not
 if [ ! -e /usr/lib/libvorbis.so.0 ] || [ ! -e /usr/lib/libvorbisfile.so.3 ]; then
 	rw
-	cp -f libvorbis.so.0 /usr/lib
-	cp -f libvorbisfile.so.3 /usr/lib
+	cp libvorbis.so.0 /usr/lib
+	cp libvorbisfile.so.3 /usr/lib
 	ro
+fi
+# Copy the shareware in /mnt/FunKey/.eduke32 for Instant Play
+if [ ! -e /mnt/FunKey/.eduke32/DUKE3D.GRP ]; then
+	if [ ! -d /mnt/FunKey/.eduke32 ]; then
+		mkdir -p /mnt/FunKey/.eduke32
+	fi
+	cp DUKE3D.GRP /mnt/FunKey/.eduke32
 fi
 # Launch EDuke32 and record pid for Instant Play
 ./eduke32 &
